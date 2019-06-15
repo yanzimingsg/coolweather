6c39b92528c314b37715a7b55ec894755ed27d4e
Mike <yanzimingsg@163.com>
2019-06-15 17:35:11 +0800

    修改程序图标和名称

--- app/src/main/java/com/example/coolweather/type/UrlType.java
+++ app/src/main/java/com/example/coolweather/type/UrlType.java
@@ -3,6 +3,6 @@ package com.example.coolweather.type;
 public class UrlType {
     public static final String  URL_ADDRESS = "http://guolin.tech/api/china";
     public static final String  URL_WEATHERID = "http://guolin.tech/api/weather?cityid=";
-    public static final String  KEY = "c52f4a615ceb4dfeb798ca0cc24e77ba";
+    public static final String  KEY = "";
     public static final String  BY_URL = "http://guolin.tech/api/bing_pic";
 }
